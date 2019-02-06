6329cc2bafeff61350bae1c667da512e9f291a46
Leroy Korterink <leroy@mediamonks.com>
2019-02-06 23:48:50 +0100

    Remove unnecessary file

--- src/components/Trees.js
+++ src/components/Trees.js
@@ -4,6 +4,15 @@ class Trees extends StaticComponent {
   constructor() {
     super();
   }
+
+  createTree(minWidth = 4, maxWidth = 8, minHeight = 10, maxHeight = 12) {
+    const width = Math.random() * (maxWidth - minWidth) + minWidth;
+    const height = Math.random() * (maxHeight - minHeight) + minHeight;
+
+    const geometry = new THREE.CylinderGeometry(0, width, height, 15, 5);
+
+    return geometry;
+  }
 }
 
 export default Trees;
