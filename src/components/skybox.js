317d4bbe6d88cdc09e43d481b2c7b66f4bde74d6
Leroy Korterink <leroy@mediamonks.com>
2019-02-06 23:33:46 +0100

    Create skybox

--- src/components/skybox.js
+++ src/components/skybox.js
@@ -2,15 +2,23 @@ import StaticComponent from "../core/StaticComponent.js";
 
 const SIZE = 1000;
 
+const directions = ["front", "back", "top", "bottom", "right", "left"];
+const createPath = direction => `assets/images/cloudtop_${direction}.png`;
+
 class Skybox extends StaticComponent {
   constructor() {
     super();
 
-    const geometry = new THREE.BoxGeometry(SIZE, SIZE, SIZE);
-    const material = new THREE.MeshNormalMaterial({
-      color: 0x00ff00,
-      side: THREE.BackSide
-    });
+    const materials = directions.map(
+      direction =>
+        new THREE.MeshBasicMaterial({
+          map: THREE.ImageUtils.loadTexture(createPath(direction)),
+          side: THREE.BackSide
+        })
+    );
+
+    const geometry = new THREE.CubeGeometry(SIZE, SIZE, SIZE);
+    const material = new THREE.MeshFaceMaterial(materials);
 
     this.object = new THREE.Mesh(geometry, material);
   }
--- src/worlds/world1.js
+++ src/worlds/world1.js
@@ -3,4 +3,10 @@ import Box from "../components/Box.js";
 import Skybox from "../components/Skybox.js";
 import Trees from "../components/Trees.js";
 
-export default [new Landscape(), new Box(), new Skybox(), new Trees()];
+export default [
+  //
+  // new Landscape(),
+  new Box(),
+  new Skybox(),
+  new Trees()
+];
