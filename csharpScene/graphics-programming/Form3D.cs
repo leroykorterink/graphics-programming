3022e17c8945ba9fda19bdd2e36a432bfeb52db0
Leroy Korterink <leroy@mediamonks.com>
2019-02-25 21:14:42 +0100

    Fix projection matrix

--- csharpScene/graphics-programming/Form3D.cs
+++ csharpScene/graphics-programming/Form3D.cs
@@ -7,9 +7,17 @@ namespace graphics_programming
 {
     public partial class Form3D : Form
     {
-        AxisX axisX;
-        AxisY axisY;
-        Cube cube;
+        private readonly AxisX axisX;
+        private readonly AxisY axisY;
+        private readonly Cube cube;
+
+        private readonly dynamic initialValues = new
+        {
+            distance = 100,
+            radius = 10,
+            theta = -100,
+            phi = -10
+        };
 
         public Form3D()
         {
@@ -24,9 +32,10 @@ namespace graphics_programming
             
             cube = new Cube(Color.Purple);
 
-            //var transformationMatrix = new Matrix4();
+            var transformationMatrix = new Matrix4();
+            transformationMatrix.RotateY(25);
 
-            //cube.ApplyMatrix(transformationMatrix);
+            cube.ApplyMatrix(transformationMatrix);
         }
 
         private List<Vector2> ViewportTransformation(float width, float height, List<Vector2> vectors)
@@ -49,9 +58,20 @@ namespace graphics_programming
 
             var distanceInverse = -distance;
 
-            vectors.ForEach(vector => result.Add(
-                new Vector2(vector.X, distance * (vector.Y / vector.Z))
-            ));
+            vectors.ForEach(vector =>
+            {
+                var projectionMatrix = new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+
+                /*var projectionMatrix = new Matrix3(
+                    -(distance / vector.Z), 0, 0,
+                    0, -(distance / vector.Z), 0,
+                    0, 0, 0
+                );*/
+
+                result.Add(
+                    projectionMatrix * new Vector2(vector.X, vector.Y)
+                );
+            });
 
             return result;
         }
@@ -60,7 +80,7 @@ namespace graphics_programming
         {
             base.OnPaint(e);
 
-            var distance = 1;
+            var distance = initialValues.distance;
 
             axisX.Draw(e.Graphics, ViewportTransformation(Width, Height, axisX.vectorBuffer));
             axisY.Draw(e.Graphics, ViewportTransformation(Width, Height, axisY.vectorBuffer));
--- csharpScene/graphics-programming/Shapes/Cube.cs
+++ csharpScene/graphics-programming/Shapes/Cube.cs
@@ -25,6 +25,7 @@ namespace graphics_programming.Shapes
 
             vectorBuffer = new List<Vector3>
             {
+                // Cube vectors
                 new Vector3( 1.0f,  1.0f, 1.0f),     // 0
                 new Vector3( 1.0f, -1.0f, 1.0f),     // 1
                 new Vector3(-1.0f, -1.0f, 1.0f),     // 2
@@ -35,6 +36,8 @@ namespace graphics_programming.Shapes
                 new Vector3(-1.0f, -1.0f, -1.0f),    // 6
                 new Vector3(-1.0f,  1.0f, -1.0f),    // 7
 
+                // Text vectors
+                /*
                 new Vector3( 1.2f,  1.2f, 1.2f),     // 0
                 new Vector3( 1.2f, -1.2f, 1.2f),     // 1
                 new Vector3(-1.2f, -1.2f, 1.2f),     // 2
@@ -44,6 +47,7 @@ namespace graphics_programming.Shapes
                 new Vector3( 1.2f, -1.2f, -1.2f),    // 5
                 new Vector3(-1.2f, -1.2f, -1.2f),    // 6
                 new Vector3(-1.2f,  1.2f, -1.2f)     // 7
+                */
             };
         }
 
@@ -55,27 +59,29 @@ namespace graphics_programming.Shapes
             g.DrawLine(_pen, vb[2].X, vb[2].Y, vb[3].X, vb[3].Y);    //2 -> 3
             g.DrawLine(_pen, vb[3].X, vb[3].Y, vb[0].X, vb[0].Y);    //3 -> 0
 
-            _pen.DashStyle = DashStyle.Dot;
-            g.DrawLine(_pen, vb[4].X, vb[4].Y, vb[5].X, vb[5].Y);    //4 -> 5
-            g.DrawLine(_pen, vb[5].X, vb[5].Y, vb[6].X, vb[6].Y);    //5 -> 6
-            g.DrawLine(_pen, vb[6].X, vb[6].Y, vb[7].X, vb[7].Y);    //6 -> 7
-            g.DrawLine(_pen, vb[7].X, vb[7].Y, vb[4].X, vb[4].Y);    //7 -> 4
+            _pen.DashStyle = DashStyle.Dash;
+            //g.DrawLine(_pen, vb[4].X, vb[4].Y, vb[5].X, vb[5].Y);    //4 -> 5
+            //g.DrawLine(_pen, vb[5].X, vb[5].Y, vb[6].X, vb[6].Y);    //5 -> 6
+            //g.DrawLine(_pen, vb[6].X, vb[6].Y, vb[7].X, vb[7].Y);    //6 -> 7
+            //g.DrawLine(_pen, vb[7].X, vb[7].Y, vb[4].X, vb[4].Y);    //7 -> 4
 
-            _pen.DashStyle = DashStyle.DashDot;
+            _pen.DashStyle = DashStyle.Dash;
             g.DrawLine(_pen, vb[0].X, vb[0].Y, vb[4].X, vb[4].Y);    //0 -> 4
-            g.DrawLine(_pen, vb[1].X, vb[1].Y, vb[5].X, vb[5].Y);    //1 -> 5
-            g.DrawLine(_pen, vb[2].X, vb[2].Y, vb[6].X, vb[6].Y);    //2 -> 6
-            g.DrawLine(_pen, vb[3].X, vb[3].Y, vb[7].X, vb[7].Y);    //3 -> 7
+            //g.DrawLine(_pen, vb[1].X, vb[1].Y, vb[5].X, vb[5].Y);    //1 -> 5
+            //g.DrawLine(_pen, vb[2].X, vb[2].Y, vb[6].X, vb[6].Y);    //2 -> 6
+            //g.DrawLine(_pen, vb[3].X, vb[3].Y, vb[7].X, vb[7].Y);    //3 -> 7
 
+            /*
             for (int i = 0; i < 8; i++)
             {
                 g.DrawString(
-                    i.ToString(), 
-                    _font, 
+                    i.ToString(),
+                    _font,
                     _pen.Brush,
                     new PointF(vb[i + 8].X, vb[i + 8].Y)
                 );
             }
+            */
         }
     }   
 }
